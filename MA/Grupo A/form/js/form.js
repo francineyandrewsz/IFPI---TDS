943773d2ad3e9a1e2f3d17bdb3362090a385cafd
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-09-06 12:23:24 -0300

    Disciplina de Metodos Ágeis

---
+++ MA/Grupo A/form/js/form.js
@@ -0,0 +1,7 @@
+function EnvOiar() {
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+}
