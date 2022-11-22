1c563e256d1a09c98ee2f0877b15ac5bd1866865
Franciney Andrews <101227997+FranNey@users.noreply.github.com>
2022-11-22 15:58:48 -0300

    Update questao3.py

--- DES OR/lista3/questao3.py
+++ DES OR/lista3/questao3.py
@@ -2,14 +2,18 @@
 retorna o valor lógico Verdadeiro caso o valor seja primo e Falso em caso
 contrário.'''
 def primo(valor):
-  if type(valor) == int and valor >= 0:
+  if type(valor) == int and valor > 0:
     for c in range(2, valor+1):
       if valor % c == 0:
-        return False
-      else:
         return True
+      else:
+        return False
   else:
     return Exception
 
-assert primo(3) == True
+assert primo(3) == False
+assert primo(4) == True
+assert primo(-2) == Exception
+assert primo(0) == Exception
+assert primo('a') == Exception
 print('Testes ok!')
